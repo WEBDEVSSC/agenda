1df0b6528e0ae64b9493a2f4cb9f1a05e652e29e
SOPORTE WEB SSC <soportewebssc@gnmail.com>
2024-08-27 14:24:00 -0600

    campos location, description en table events

---
+++ database/migrations/2024_08_27_202308_add_location_and_description_to_events_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('events', function (Blueprint $table) {
+            //
+            $table->string('location')->nullable(); // Agrega el campo location
+            $table->text('description')->nullable(); // Agrega el campo description
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('events', function (Blueprint $table) {
+            //
+            $table->dropColumn(['location', 'description']); // Elimina los campos location y description
+        });
+    }
+};
